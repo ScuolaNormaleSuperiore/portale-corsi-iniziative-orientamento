396bb828332f458db00db3efd03b066595a14f6f
Giacomo Terreni <giacomo.terreni@gmail.com>
2024-11-07 16:56:52 +0100

    news ed eventi in home apge step 1

--- app/Http/Controllers/FEController.php
+++ app/Http/Controllers/FEController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
+use App\Models\News;
 use App\Models\Pagina;
 use Igaster\LaravelTheme\Facades\Theme;
 use Illuminate\Http\Request;
@@ -28,9 +30,24 @@ class FEController extends Controller
         $pagine = Pagina::where('attivo',1)
             ->where('homepage',1)
             ->orderBy('ordine','ASC')
+            ->limit(6)
             ->get();
 
-        return view('index',compact('pagine'));
+        $news = News::where('attivo',1)
+            ->whereNotNull('evidenza')
+            ->orderBy('evidenza','ASC')
+            ->get();
+
+        $newsAlta = $news->where('evidenza',1)->first();
+
+        $newsBasse = $news->where('evidenza','>',1)->all();
+
+        $eventi = Evento::where('attivo',1)
+            ->whereNotNull('evidenza')
+            ->orderBy('evidenza','ASC')
+            ->get();
+
+        return view('index',compact('pagine','newsAlta', 'newsBasse', 'eventi'));
     }
 
     public function paginaOrientamento(Request $request, Pagina $pagina)
--- resources/views/sns/index.blade.php
+++ resources/views/sns/index.blade.php
@@ -80,31 +80,184 @@
             </div>
         </section>
 
+        {{--        Pagine orientamento--}}
+
         <section style="background-color:#F4FAFB" class="pt-5">
 
             <div class="container">
                 <h2>Partecipa alle nostre attività di orientamento</h2>
-                <p class="" style="color:#2F475E;">Scopri le opportunità offerte per partecipare ad eventi di orientamento, visitare i luoghi della Normale e vivere esperienze educative uniche.</p>
-                <div class="row">
+                <p class="" style="color:#2F475E;">Scopri le opportunità offerte per partecipare ad eventi di
+                    orientamento, visitare i luoghi della Normale e vivere esperienze educative uniche.</p>
+                <div class="row pb-5">
                     <div class="card-wrapper card-teaser-wrapper card-teaser-block-3">
                         <!--start card-->
                         @foreach($pagine as $pagina)
-                        <div class="card card-teaser" style="border-radius: 4px;border-top: 3px solid #005A74;">
-                            <div class="card-body" >
+                            <div class="card card-teaser" style="border-radius: 4px;border-top: 3px solid #005A74;">
+                                <div class="card-body">
                                     <h5 class="card-title h5 text-primary">{{$pagina->titolo_it}}</h5>
-                                <p class="card-text font-serif">
-                                    <a href="/pagina-orientamento/{{$pagina->id}}">
-                                    Scopri di più
-                                    <svg class="icon icon-primary">
-                                        <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
-                                    </svg>
-                                    </a>
-                                </p>
+                                    <p class="card-text font-serif">
+                                        <a href="/pagina-orientamento/{{$pagina->id}}">
+                                            Scopri di più
+                                            <svg class="icon icon-primary">
+                                                <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
+                                            </svg>
+                                        </a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
                         @endforeach
 
                     </div>
+                </div>
+
+                <div class="d-flex justify-content-end pb-5">
+                    <button type="button" class="btn btn-outline-primary">
+                        Scopri l'orientamento
+                        <svg class="icon icon-primary">
+                            <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        {{--        News--}}
+
+        <section class="pt-5">
+            <div class="container">
+                <h2 class="pb-5">In evidenza</h2>
+
+
+                <div class="row">
+
+                    @if($newsAlta)
+                        <div class="col-12 pb-5">
+                            <!--start card-->
+                            <div class="card-wrapper">
+                                <div class="card card-bg card-img no-after">
+                                    <div class="card-body d-flex p-0">
+                                        <div class="img-responsive-wrapper w-50">
+                                            <div class="img-responsive img-responsive-panoramic">
+                                                <figure class="img-wrapper">
+                                                    <img src="{{$newsAlta->picture}}"
+                                                         title="{{$newsAlta->titolo_it}}"
+                                                         alt="{{$newsAlta->titolo_it}}">
+                                                </figure>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-column justify-content-center w-50 align-items-center">
+                                            <h3 class="card-title h5 ">{{$newsAlta->titolo_it}}</h3>
+                                            <p class="card-text font-serif">{{$newsAlta->sottotitolo_it}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
+                    @endif
+                </div>
+                <div class="row pb-5">
+
+                    @foreach ($newsBasse as $newsBassa)
+                        <div class="col-12 col-lg-4">
+                            <!--start card-->
+                            <div class="card-wrapper">
+                                <div class="card card-bg  card-img no-after">
+                                    <div class="img-responsive-wrapper">
+                                        <div class="img-responsive img-responsive-panoramic">
+                                            <figure class="img-wrapper">
+                                                <img src="{{$newsBassa->picture}}"
+                                                     title="{{$newsBassa->titolo_it}}" alt="{{$newsBassa->titolo_it}}">
+                                            </figure>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <h3 class="card-title h5 ">{{$newsBassa->titolo_it}}</h3>
+                                        {{--                                        <p class="card-text font-serif"></p>--}}
+                                        {{--                                        <a class="read-more" href="#">--}}
+                                        {{--                                            <span class="text">Leggi di più</span>--}}
+                                        {{--                                            <span class="visually-hidden">su Lorem ipsum dolor sit amet, consectetur adipiscing elit…</span>--}}
+                                        {{--                                            <svg class="icon">--}}
+                                        {{--                                                <use href="/bootstrap-italia/dist/svg/sprites.svg#it-arrow-right"></use>--}}
+                                        {{--                                            </svg>--}}
+                                        {{--                                        </a>--}}
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
+                    @endforeach
+
+                </div>
+
+                <div class="d-flex justify-content-end pb-5">
+                    <button type="button" class="btn btn-outline-primary">
+                        Tutte le notizie
+                        <svg class="icon icon-primary">
+                            <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        {{--        Eventi--}}
+
+        <section class="pt-5">
+            <div class="container">
+                <h2 class="pb-5">Eventi</h2>
+
+
+                <div class="row pb-5">
+
+                    @foreach ($eventi as $evento)
+                        <div class="col-12 col-lg-4">
+                            <!--start card-->
+                            <div class="card-wrapper">
+                                <div class="card card-bg  card-img no-after">
+                                    <div class="img-responsive-wrapper">
+                                        <div class="img-responsive img-responsive">
+                                            <figure class="img-wrapper">
+                                                <img src="{{$evento->picture}}"
+                                                     title="{{$evento->titolo_it}}" alt="{{$evento->titolo_it}}">
+                                            </figure>
+                                            @if ($evento->data)
+                                                @php
+                                                    $date = \Carbon\Carbon::createFromFormat("Y-m-d",$evento->data)->locale('it_IT');
+                                                @endphp
+                                                <div class="card-calendar d-flex flex-column justify-content-center">
+                                                    <span class="card-date">{{$date->day}}</span>
+                                                    <span class="card-day">{{$date->monthName}}</span>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <h3 class="card-title h5 ">{{$evento->titolo_it}}</h3>
+                                        {{--                                        <p class="card-text font-serif"></p>--}}
+                                        {{--                                        <a class="read-more" href="#">--}}
+                                        {{--                                            <span class="text">Leggi di più</span>--}}
+                                        {{--                                            <span class="visually-hidden">su Lorem ipsum dolor sit amet, consectetur adipiscing elit…</span>--}}
+                                        {{--                                            <svg class="icon">--}}
+                                        {{--                                                <use href="/bootstrap-italia/dist/svg/sprites.svg#it-arrow-right"></use>--}}
+                                        {{--                                            </svg>--}}
+                                        {{--                                        </a>--}}
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
+                    @endforeach
+
+                </div>
+
+                <div class="d-flex justify-content-end pb-5">
+                    <button type="button" class="btn btn-outline-primary">
+                        Tutte gli eventi
+                        <svg class="icon icon-primary">
+                            <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </section>
