0fd551a835d3cda89719f9297bb5227760f6ceb9
Giacomo Terreni <giacomo.terreni@gmail.com>
2024-11-13 16:05:05 +0100

    sportello studenti migiorato

--- resources/views/sns/components/card-orientamento.blade.php
+++ resources/views/sns/components/card-orientamento.blade.php
@@ -1,9 +1,9 @@
 <div class="card card-bg rounded card-teaser bg-white card-orientamento"
      style="border-top: 3px solid #005A74;">
     <div class="card-body">
-        <h5 class="card-title h5 text-primary">{{$pagina->titolo_it}}</h5>
+        <h5 class="card-title h5 text-primary">{{$pagina->titolo_it ?: $pagina->nome_it }}</h5>
         <p class="card-text font-sans-serif">
-            <a href="/pagina-orientamento/{{$pagina->id}}" class="fw-semibold">
+            <a href="/{{isset($baselink) ? $baselink : 'pagina-orientamento'}}/{{$pagina->id}}" class="fw-semibold">
                 Scopri di più
                 <svg class="icon icon-primary icon-sm">
                     <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
--- resources/views/sns/sportello-studenti.blade.php
+++ resources/views/sns/sportello-studenti.blade.php
@@ -14,21 +14,8 @@
                 <div class="card-wrapper card-teaser-wrapper card-teaser-block-3">
                     <!--start card-->
                     @foreach($classi as $classe)
-                        <div class="card card-bg  card-teaser" style="border-radius: 4px;border-top: 3px solid #005A74;">
-                            <div class="card-body">
-                                <h5 class="card-title h5 text-primary">{{$classe->nome_it}}</h5>
-                                <p class="card-text font-serif">
-                                    <a href="/sportello-studenti/{{$classe->id}}">
-                                        Scopri di più
-                                        <svg class="icon icon-primary">
-                                            <use href="{{Theme::url('svg/sprites.svg')}}#it-arrow-right"></use>
-                                        </svg>
-                                    </a>
-                                </p>
-                            </div>
-                        </div>
+                        @include('components.card-orientamento',['pagina' => $classe, 'baselink' => 'sportello-studenti'])
                     @endforeach
-
                 </div>
             </section>
         </div>
