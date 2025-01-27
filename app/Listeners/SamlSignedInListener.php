7147451cef103fe56020955fbedb5df2e2d0b45d
Massimiliano Pardini <pardini@netseven.it>
2025-01-27 17:18:57 +0100

    Modifiche per redirect

--- app/Listeners/SamlSignedInListener.php
+++ app/Listeners/SamlSignedInListener.php
@@ -33,14 +33,13 @@ class SamlSignedInListener
         ];
         Log::info('SAML User authenticated', $userData);
 
-        $userEmail = Arr::get(Arr::get($attributes,'spidEmail'),0);
+        $userEmail = Arr::get(Arr::get($attributes, 'spidEmail'), 0);
 
         $user = $userEmail ? User::where('email', $userEmail)->first() : null;
 
         if ($user) {
             // Login the user
-            $this->login($user);
-
+            return $this->login($user);
         } elseif ($userEmail) {
             // Generate a random password
             $randomPassword = Str::random(12);
@@ -49,25 +48,26 @@ class SamlSignedInListener
                 'name' => $userEmail,
                 'email' => $userEmail,
                 'password' => \bcrypt($randomPassword),
-                'nome' => Arr::get(Arr::get($attributes,'spidName'),0),
-                'cognome' => Arr::get(Arr::get($attributes,'spidFamilyName'),0),
+                'nome' => Arr::get(Arr::get($attributes, 'spidName'), 0),
+                'cognome' => Arr::get(Arr::get($attributes, 'spidFamilyName'), 0),
                 'info' => $attributes,
                 'email_verified_at' => now()->toDateTimeString(),
             ];
             $user = User::create($userData);
             $user->assignRole('Studente');
-            $this->login($user);
+            return $this->login($user);
         } else {
             return redirect()->intended(RouteServiceProvider::HOME);
             //NO EMAIL DA GESTIRE
         }
     }
 
-    protected function login(User $user) {
+    protected function login(User $user)
+    {
         Auth::login($user);
         $token = $user->createToken('auth_token')->plainTextToken;
         request()->session()->regenerate();
-        request()->session()->put('sanctum_token',$token);
+        request()->session()->put('sanctum_token', $token);
         if (!Auth::user()->hasVerifiedEmail()) {
             return redirect()->intended(route('verification.notice'));
         }
