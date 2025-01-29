5247867104a8c81e3d1a70e0c8c522e736164615
Giacomo Terreni <giacomoterreni@gmail.com>
2025-01-29 17:00:19 +0100

    bugfix e emails

--- resources/views/roma-vue/includes/head.blade.php
+++ resources/views/roma-vue/includes/head.blade.php
@@ -2,7 +2,7 @@
 
 <meta name=bearer-token content={{Session::get('sanctum_token')}}>
 
-<head>
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="icon" href="/roma-vue/favicon.ico">
@@ -15,8 +15,8 @@
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
 
-  <script type="module" crossorigin src="/roma-vue/assets/index-40b98aca.js"></script>
-  <link rel="stylesheet" href="/roma-vue/assets/index-bc235ef4.css">
-</head>
+  <script type="module" crossorigin src="/roma-vue/assets/index-875dd41f.js"></script>
+  <link rel="stylesheet" href="/roma-vue/assets/index-77f56378.css">
+
 </head>
 
