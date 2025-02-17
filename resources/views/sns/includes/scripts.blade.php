c19bc5f6be8120eff20711f8a31429b03d1a9ffb
Giacomo Terreni <giacomoterreni@gmail.com>
2025-02-17 16:22:08 +0100

    matomo

--- resources/views/sns/includes/scripts.blade.php
+++ resources/views/sns/includes/scripts.blade.php
@@ -176,4 +176,19 @@
 }
 </style>
 
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="https://ingestion.webanalytics.italia.it/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '41424']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<!-- End Matomo Code -->
+
 @yield('extra-scripts')
