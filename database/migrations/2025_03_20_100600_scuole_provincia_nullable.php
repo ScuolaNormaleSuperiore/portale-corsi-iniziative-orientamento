61bc3cb7fe2e32a11c4ab485fb1184b84a9bbfbf
Giacomo Terreni <giacomoterreni@gmail.com>
2025-03-20 15:17:30 +0100

    scuola provinica

---
+++ database/migrations/2025_03_20_100600_scuole_provincia_nullable.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+
+    public function up()
+    {
+
+        Schema::table('scuole', function (Blueprint $table) {
+
+            $table->string('provincia_id')->nullable(true)->change();
+
+        });
+
+    }
+
+    public function down()
+    {
+
+    }
+};
